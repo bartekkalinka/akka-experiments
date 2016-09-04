c1d1a4093718253dfd38f4fa148eef7b5cf62d8d
Bartek Kalinka <bartkali@hotmail.com>
2016-09-04 13:59:35 +0200

    producer call test

--- build.sbt
+++ build.sbt
@@ -8,6 +8,7 @@ libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor" % "2.4.9",
   "com.typesafe" % "config" % "1.3.0",
   "com.typesafe.akka" %% "akka-testkit" % "2.4.9",
-  "org.scalatest" %% "scalatest" % "3.0.0" % "test"
+  "org.scalatest" %% "scalatest" % "3.0.0" % "test",
+  "org.mockito" % "mockito-core" % "2.1.0-beta.120" % "test"
 )
 
--- src/test/scala/edu/MultipleCallsActorSpec.scala
+++ src/test/scala/edu/MultipleCallsActorSpec.scala
@@ -3,13 +3,15 @@ package edu
 import akka.actor.ActorSystem
 import akka.testkit.{ImplicitSender, TestKit}
 import org.scalatest.{FlatSpecLike, Matchers}
-
 import scala.concurrent.Future
+import org.mockito.Mockito._
+import org.scalatest.mockito.MockitoSugar
 
 class MultipleCallsActorSpec extends TestKit(ActorSystem("MultipleCallsActorSpec"))
   with ImplicitSender
   with FlatSpecLike
-  with Matchers {
+  with Matchers
+  with MockitoSugar {
 
   import MultipleCallsActor._
 
@@ -59,8 +61,16 @@ class MultipleCallsActorSpec extends TestKit(ActorSystem("MultipleCallsActorSpec
     expectMsg(Map[Int, CallResponse](1 -> MyCallResponse("q")))
   }
 
+  it should "actually call producer send method" in {
+    val calls: Seq[MyCallRequest] = Seq(MyCallRequest(1, "a"), MyCallRequest(2, "b"))
+    val mockProducer = mock[CallProducer]
+    val multipleCallsActor = system.actorOf(MultipleCallsActor.props(mockProducer))
+    multipleCallsActor ! HandleCalls(calls)
+    verify(mockProducer).send(MyCallRequest(1, "a"))
+    verify(mockProducer).send(MyCallRequest(2, "b"))
+  }
+
   //TODO timeout for getting all responses
-  //TODO test that CallProducer.send is called
   //TODO test incorrect ids in handle response
 }
 
